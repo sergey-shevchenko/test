0b4547b2ab8bd752aa3121786b7b2d6700c45434
sergey-shevchenko <charlie-2503>
2014-01-09 18:21:29 +0200

    extended config options

--- config-sample.php
+++ config-sample.php
@@ -14,3 +14,13 @@ define( 'RS_USER', 'admin' );
  * Rackspace authorisation SSH key name. Default is 'adminKey'
  */
 define( 'RS_KEY', 'adminKey' );
+
+/**
+ * Define whether use RackSpace or OpenStack API. Acceptable parameters are 'rackspace and openstack'
+ */
+define( 'OS_TYPE', 'openstack' );
+
+/**
+ * OpenStack password. Required if you use OpenStack API. Default is 'Passw0rd'
+ */
+define( 'OS_PASS', 'Passw0rd' );
--- index.php
+++ index.php
@@ -3,8 +3,26 @@ require 'vendor/autoload.php';
 require 'config.php';
 
 use OpenCloud\Rackspace;
+use OpenCloud\OpenStack;
 
-$client = new Rackspace( RS_HOST, array(
-    'username' => RS_USER,
-    'apiKey'   => RS_KEY
-)  );
+/**
+ * Required to accept self-signed certificates
+ */
+
+if ( 'rackspace' == OS_TYPE ) {
+    $client = new Rackspace( RS_HOST, array(
+        'username' => RS_USER,
+        'apiKey'   => RS_KEY
+    )  );
+} else {
+    $client = new OpenStack( RS_HOST, array(
+        'username' => RS_USER,
+        'password' => OS_PASS
+    ) );
+}
+
+$compute = $client->computeService( '', 'ORL' );
+
+$images = $compute->imageList();
+
+var_dump( $images );
