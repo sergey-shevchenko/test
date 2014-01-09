c49a0f4eb3684f64c072da12ce1c6e5481813c9b
sergey-shevchenko <charlie-2503>
2014-01-09 15:53:32 +0200

    simple config files

---
+++ config-sample.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Rackspace host, look for in in greeting message on your VM with RackSpace Sandbox
+ */
+define( 'RS_HOST', '' );
+
+/**
+ * Rackspace username. Default is 'admin'
+ */
+define( 'RS_USER', 'admin' );
+
+/**
+ * Rackspace authorisation SSH key name. Default is 'adminKey'
+ */
+define( 'RS_KEY', 'adminKey' );
--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 require 'vendor/autoload.php';
+require 'config.php';
+
+use OpenCloud\Rackspace;
+
+$client = new Rackspace( RS_HOST, array(
+    'username' => RS_USER,
+    'apiKey'   => RS_KEY
+)  );
